45b44defcfdafe2b3472211376f939ebb1770881
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2023-05-16 15:52:29 +0200

    drawing if dd4hep output format

--- arcfullsim.py
+++ arcfullsim.py
@@ -98,17 +98,18 @@ if __name__ == "__main__":
         SIM.run()
         if os.path.getsize( SIM.outputFile ) < 1000000 :
             raise RuntimeError("Output file not found or size less than 1MB")
-        # Create some images
-        outImagePrefix = "arcsim_"
-        rootfile = ROOT.TFile(SIM.outputFile)
-        EVENT = rootfile.Get("EVENT")
-        EVENT.Draw("ARC_HITS.position.Z():ARC_HITS.position.phi()","((ARC_HITS.cellID>>5)&0x7)==0")
-        ROOT.gPad.SaveAs( outImagePrefix + "barrel_" + SIM.gun.particle + ".png")
-        EVENT.Draw("ARC_HITS.position.X():ARC_HITS.position.Y()","((ARC_HITS.cellID>>5)&0x7)==1")
-        ROOT.gPad.SaveAs( outImagePrefix + "endcapZpos_" + SIM.gun.particle + ".png")
-        EVENT.Draw("ARC_HITS.position.X():ARC_HITS.position.Y()","((ARC_HITS.cellID>>5)&0x7)==2")
-        ROOT.gPad.SaveAs( outImagePrefix + "endcapZneg_" + SIM.gun.particle + ".png")
-        rootfile.Close()
+        if not "edm4hep" in SIM.outputFile :
+            # Create some images
+            outImagePrefix = "arcsim_"
+            rootfile = ROOT.TFile(SIM.outputFile)
+            EVENT = rootfile.Get("EVENT")
+            EVENT.Draw("ARC_HITS.position.Z():ARC_HITS.position.phi()","((ARC_HITS.cellID>>5)&0x7)==0")
+            ROOT.gPad.SaveAs( outImagePrefix + "barrel_" + SIM.gun.particle + ".png")
+            EVENT.Draw("ARC_HITS.position.X():ARC_HITS.position.Y()","((ARC_HITS.cellID>>5)&0x7)==1")
+            ROOT.gPad.SaveAs( outImagePrefix + "endcapZpos_" + SIM.gun.particle + ".png")
+            EVENT.Draw("ARC_HITS.position.X():ARC_HITS.position.Y()","((ARC_HITS.cellID>>5)&0x7)==2")
+            ROOT.gPad.SaveAs( outImagePrefix + "endcapZneg_" + SIM.gun.particle + ".png")
+            rootfile.Close()
 
         logger.info("TEST: passed")
 
